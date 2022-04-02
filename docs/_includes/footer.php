7f8ca4581c4bd4aa69018d1bf73961e1664e8984
Elshindr <headkeytics@gmail.com>
2022-04-02 21:17:55 +0200

    footer section

---
+++ docs/_includes/footer.php
@@ -0,0 +1,50 @@
+<footer class="footer">
+    <div class="container ">
+      <a href="#hero" class="back-to-top" aria-label="go back to top">
+        <i class="fa fa-angle-up fa-2x" aria-hidden="true"></i>
+      </a>
+      <div class="social-links">
+        <a
+          href="https://twitter.com/elshindr"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="twitter"
+        >
+          <i class="fa fa-twitter"></i>
+        </a>
+       
+        <a
+          href="https://www.linkedin.com/in/noelinemarie/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="linkedin"
+        >
+          <i class="fa fa-linkedin"></i>
+        </a>
+        <a
+          href="https://github.com/Elshindr"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="github"
+        >
+          <i class="fa fa-github"></i>
+        </a>
+      </div>
+      <hr />
+      <p class="footer__text">
+        &copy; <span id="year"></span> - Template by
+        <a
+          href="https://github.com/AnilSeervi"
+          target="_blank"
+          rel="noopener noreferrer"
+          >Anil Seervi</a
+        >. <br />Made with &hearts;
+      </p>
+    </div>
+  </footer>
+  <script>
+    document.getElementById("year").textContent = new Date().getFullYear();
+  </script>
+  <script src="./javascript/vanilla-tilt.min.js"></script>
+  <script type="text/javascript" src="./javascript/valtilt.js"></script>
+</body>
