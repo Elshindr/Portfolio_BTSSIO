62e1f30c414d8d89ca3fc67e01d6ae8afde95165
Elshindr <headkeytics@gmail.com>
2022-05-01 15:09:21 +0200

    tp 12 chocolatein performance cloud pca

---
+++ 12_chocolatein_Performance/datatest.sh
@@ -0,0 +1,7 @@
+ab -n 100 -c 10 -g donnees1.data https://chocolatein.gil83.fr/ &
+ab -n 100 -c 10 -g donnees2.data https://chocolatein.gil83.fr/ &
+ab -n 100 -c 10 -g donnees3.data https://chocolatein.gil83.fr/ &
+ab -n 100 -c 10 -g donnees4.data https://chocolatein.gil83.fr/ &
+ab -n 100 -c 10 -g donnees5.data https://chocolatein.gil83.fr/ &
+ab -n 100 -c 10 -g donnees6.data https://chocolatein.gil83.fr/ &
+ab -n 100 -c 10 -g donnees7.data https://chocolatein.gil83.fr/ &
